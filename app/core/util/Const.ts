99ab412517e9443bcbccea9b3bbb3c7bcc7611e7
Tarun Juneja <tarun.juneja@barco.com>
2017-06-14 11:55:37 +0530

    Fixing problem with the setup

--- app/core/util/Const.ts
+++
@@ -1,9 +0,0 @@
-export
-/**
- * @class Const
- * Holds core level constant
- * Shouldn't hold any app level constants
- */
-class Const {
-    public static IPRegex: RegExp = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/;     
-}
