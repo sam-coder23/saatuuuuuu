7ec9b0cc6b1781324862e22f352dd1344eea539a
Juneja, Tarun <Tarun.Juneja@barco.com>
2017-10-27 13:16:09 +0200

    Merge pull request #29 in OCS/launchpad-cr from feature/TSM8889-1060 to master
    TSM8889-1060: Making test case for user model available
    
    Squashed commit of the following:
    
    commit 7fa3f31a870d34d00a5f8d1338b6ed9082ccd21c
    Author: saransh <saransh.kumar@barco.com>
    Date:   Fri Oct 27 15:47:45 2017 +0530
    
        TSM8889-1060: Implemented review remarks.
    
    commit 3683020bd6847fc5ad7986a0630cf7d816db3eed
    Author: saransh <saransh.kumar@barco.com>
    Date:   Fri Oct 27 15:44:35 2017 +0530
    
        TSM8889-1060: Some improvement in Unit test cases for user Model.
    
    commit 5e919a0d5d20cdf25771e3fc1aa8c46aac928e73
    Author: saransh <saransh.kumar@barco.com>
    Date:   Fri Oct 27 12:29:16 2017 +0530
    
        TSM8889-1060: Unit test cases for user Model.

---
+++ app/launchpad/models/cms-user.model.spec.ts
@@ -0,0 +1,58 @@
+import { TestBed, async } from "@angular/core/testing";
+import { User, UserConfig } from "./cms-user.model";
+
+describe("User Model: ", () => {
+
+    let user, expectedLoggedInState = true;
+
+    let expectedUserConfig: UserConfig = {
+        username: "bcd-se-test",
+        password: "bcdsetest"
+    };
+
+    let expectedSerializableObj = {
+        username: "bcd-se-test",
+        loggedIn: expectedLoggedInState
+    };
+
+    beforeEach(async () =>
+        TestBed.configureTestingModule({}));
+
+    beforeEach(() => {
+        user = new User(expectedUserConfig);
+    });
+
+    it("should be defined", () => {
+        expect(user).toBeDefined();
+        expect(user.username).toBe(expectedUserConfig.username);
+        expect(user.password).toBe(expectedUserConfig.password);
+    });
+
+    it("should set and get LoggedIn method", () => {
+        // LoggedIn setter to set loggedIn property
+        user.LoggedIn = expectedLoggedInState;
+        // Expect loggedIn property to be set right
+        expect(user.loggedIn).toBe(expectedLoggedInState);
+        // Expect LoggedIn getter method to return the right state of login
+        expect(user.LoggedIn).toBe(expectedLoggedInState);
+    });
+
+    it("should get UserName and Password", () => {
+        expect(user.Username).toBe(expectedUserConfig.username);
+        expect(user.Password).toBe(expectedUserConfig.password);
+    });
+
+    it("should return expected object from asSerializable method", () => {
+        // LoggedIn setter to set loggedIn property
+        user.LoggedIn = expectedLoggedInState;
+
+        let serializableObj = user.asSerializable();
+        expect(serializableObj).toEqual(expectedSerializableObj);
+    });
+
+    it("should return expected object from toJSON method", () => {
+        let userJSON = user.toJSON();
+        expect(userJSON).toEqual(expectedUserConfig);
+    });
+
+});
