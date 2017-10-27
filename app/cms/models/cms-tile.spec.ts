0fc529d4c037083a3dfd4a5d4501a3b589e891b7
Juneja, Tarun <Tarun.Juneja@barco.com>
2017-10-27 13:19:55 +0200

    Merge pull request #30 in OCS/launchpad-cr from feature/TSM8889-1038 to master
    TSM8889-1038: Making unit test case for Tile model available to master
    
    Squashed commit of the following:
    
    commit 7409f286a824429b3e814d2e7fe6b125acda2a0a
    Author: vikso <vikaschandra.soni@barco.com>
    Date:   Fri Oct 27 15:56:12 2017 +0530
    
        cms tile changes as per comments
    
    commit ad1ad396136c51338783f46833d5968809b87acb
    Author: vikso <vikaschandra.soni@barco.com>
    Date:   Fri Oct 27 12:29:11 2017 +0530
    
        Unit Test Tile Model TSM8889-1066

---
+++ app/cms/models/cms-tile.spec.ts
@@ -0,0 +1,45 @@
+import { Tile } from "./cms-tile";
+
+let tileModel = {
+    "x": 0.7279344858962693,
+    "y": 1.380379469551444,
+    "width": 2048,
+    "height": 1080
+};
+
+describe("Tile Model - ", () => {
+    it("should set and get Tile Model attributes ", () => {
+        let tile = new Tile(tileModel);
+        expect(tile.x).toBe(tileModel.x);
+        expect(tile.y).toBe(tileModel.y);
+        expect(tile.width).toBe(tileModel.width);
+        expect(tile.height).toBe(tileModel.height);
+        /** setter */
+        let xPosition = 0.50, yPosition = 2.50;
+        tile.x = xPosition;
+        tile.y = yPosition;
+        expect(tile.x).toEqual(xPosition);
+        expect(tile.y).toEqual(yPosition);
+    });
+
+    it("should set and get null Tile Model attributes ", () => {
+        let tile = new Tile({
+            "x": null,
+            "y": null,
+            "width": null,
+            "height": null 
+        });
+        expect(tile.x).toBeNull();
+        expect(tile.y).toBeNull();
+        expect(tile.width).toBeNull();
+        expect(tile.height).toBeNull();
+    });
+
+    it("should check for undefined tile model ", () => {
+        let tile = new Tile();
+        expect(tile.x).toBeUndefined();
+        expect(tile.y).toBeUndefined();
+        expect(tile.width).toBeUndefined();
+        expect(tile.height).toBeUndefined();
+    });
+});
