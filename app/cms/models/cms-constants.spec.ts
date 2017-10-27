35253a70c34079409973212857fe7f09d1cea3ab
Juneja, Tarun <Tarun.Juneja@barco.com>
2017-10-27 13:16:45 +0200

    Merge pull request #31 in OCS/launchpad-cr from feature/TSM8889-1063 to master
    TSM8889-1063: Making test case for CMS constant available
    
    Squashed commit of the following:
    
    commit 33d92e312511d7a758a2a15184b58f7e010e3108
    Author: BARCO\ANKBH <ankit.bhardwaj@barco.com>
    Date:   Fri Oct 27 12:33:11 2017 +0530
    
        TSM8889-1063: Developed Test Cases

---
+++ app/cms/models/cms-constants.spec.ts
@@ -0,0 +1,16 @@
+import { ComponentFixture, TestBed, async, inject } from "@angular/core/testing";
+import { CMSConstants } from "../../cms/models/cms-constants";
+
+describe("CMSConstants", () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [CMSConstants]
+        })
+    });
+    it("should expect the static values not to be changed", async(() => {
+      expect(CMSConstants.NoDisplay).toEqual("nodisplay");
+      expect(typeof CMSConstants.RTLLanguages).not.toBeUndefined();
+      expect(CMSConstants.RTLLanguages).toContain("ar");
+      expect(CMSConstants.DisplayUpdated).toContain("DisplayUpdated");
+    }));
+});
