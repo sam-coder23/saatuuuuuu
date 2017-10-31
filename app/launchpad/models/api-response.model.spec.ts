556293fb3e39693f1821ac438101adf464cae5c4
Tarun Juneja <tarun.juneja@barco.com>
2017-10-31 17:11:08 +0530

    TSM8889-1061: Making test case api respons model available

---
+++ app/launchpad/models/api-response.model.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed, async } from "@angular/core/testing";
+import { APIResponse } from "./api-response.model";
+
+describe("API-Response Model: ", () => {
+
+    let apiResponse;
+    let expectedStatus = true;
+    let expectedErrorMessage = "Some Error Message.";
+    let expectedResponse = {};
+
+    beforeEach(async () =>
+        TestBed.configureTestingModule({}));
+
+    beforeEach(() => {
+        apiResponse = new APIResponse(expectedStatus, expectedErrorMessage, expectedResponse);
+    });
+
+    it("should be defined", () => {
+        expect(apiResponse).toBeDefined();
+        expect(apiResponse.status).toBe(expectedStatus);
+        expect(apiResponse.errorMessage).toBe(expectedErrorMessage);
+        expect(apiResponse.response).toBe(expectedResponse);
+    });
+
+    it("should get Status, ErrorMessage and Response", () => {
+        expect(apiResponse.Status).toBe(expectedStatus);
+        expect(apiResponse.ErrorMessage).toBe(expectedErrorMessage);
+        expect(apiResponse.Response).toBe(expectedResponse);
+    });
+
+    it("should return empty object from asSerializable method", () => {
+        expect(apiResponse.asSerializable()).toEqual({});
+    });
+
+    it("should return empty object from toJSON method", () => {
+        expect(apiResponse.toJSON()).toEqual({});
+    });
+
+});
