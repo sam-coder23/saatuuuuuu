192251f365ab2f338a481d06bcae3255a68be5aa
Tarun Juneja <tarun.juneja@barco.com>
2017-11-01 15:17:15 +0530

    TSM8889-1064: Making unit test for display model available

---
+++ app/cms/models/cms-display.spec.ts
@@ -0,0 +1,72 @@
+import { ComponentFixture, TestBed, async, inject } from "@angular/core/testing";
+import { Display } from "../../cms/models/cms-display";
+import { TilePresets } from "../../shared/tile-grid/tile-grid.mock";
+
+
+let displayModel = {
+    "type": "NGPWALL",
+    "online": true,
+    "tilerId": 2048112,
+    "resolution": {
+        "width": 1024,
+        "height": 512
+    },
+    "tiles": [{
+        "left": 0,
+        "top": 0,
+        "width": 682.6666666666666,
+        "height": 360
+    },
+    {
+        "left": 682.6666666666666,
+        "top": 0,
+        "width": 682.6666666666666,
+        "height": 360
+    }],
+    "content": []
+};
+
+describe("display Model - ", () => {
+    
+    it("should set and get display Model attributes ", () => {
+        let display = new Display(displayModel);
+        expect(display.type).toBe(displayModel.type);
+        expect(display.online).toBe(displayModel.online);
+        expect(display.resolution.width).toBe(displayModel.resolution.width);
+        expect(display.resolution.height).toBe(displayModel.resolution.height);
+        expect(display.tilerId).toBe(displayModel.tilerId);
+
+        expect(display.tiles[0].left).toEqual(displayModel.tiles[0].left);
+        expect(display.tiles[0].top).toEqual(displayModel.tiles[0].top);
+        expect(display.tiles[0].width).toEqual(displayModel.tiles[0].width);
+        expect(display.tiles[0].height).toEqual(displayModel.tiles[0].height);
+    });
+
+    it("should set and get null display Model attributes ", () => {
+        let display = new Display({
+            "type": null,
+            "online": false,
+            "tilerId": null,
+            "resolution": null,
+            "tiles": [],
+            "content": []
+        });
+        expect(display.type).toBeNull();
+        expect(display.online).toBeFalsy();
+        expect(display.resolution).toBeNull();
+        expect(display.tilerId).toBeNull();
+        expect(display.tiles).toEqual([]);
+        expect(display.content).toEqual([]);
+    });
+
+
+    it("should check for undefined display model ", () => {
+        let display = new Display({});
+        expect(display.type).toBeUndefined();
+        expect(display.online).toBeUndefined();
+        expect(display.resolution).toBeUndefined();
+        expect(display.tilerId).toBeUndefined();
+        expect(display.tiles).toBeUndefined([]);
+        expect(display.content).toBeUndefined([]);
+    });
+});
