553e3a9a2f3a862947edb906f2baa685352ea116
Tarun Juneja <tarun.juneja@barco.com>
2017-11-22 09:58:04 +0530

    TSM8889-1069: Making event emitter service available

---
+++ app/cms/api/cms-event-emitter.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from "@angular/core/testing";
+import { CmsEventEmitterService } from "./cms-event-emitter.service";
+import { CMS_EVENTS } from "./cms-events.enum";
+
+describe("Service: cms-event-emitter", () => {
+
+    it("Registered for MiniDisplay events", () => {
+        CmsEventEmitterService.get(CMS_EVENTS.MiniDisplay);
+        expect((<any>CmsEventEmitterService).emitters[0]).toBeDefined();
+    });
+
+    it("Registered for TileList events", () => {
+        CmsEventEmitterService.get(CMS_EVENTS.TileList);
+        expect((<any>CmsEventEmitterService).emitters[5]).toBeDefined();
+    });
+
+    it("Not registered for Display events", () => {
+        expect((<any>CmsEventEmitterService).emitters[1]).toBeFalsy();
+    });
+});
