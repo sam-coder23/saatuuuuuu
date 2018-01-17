f2027cceb956a25833f28c8946e3ed982db246b2
Tarun Juneja <tarun.juneja@barco.com>
2018-01-17 11:56:21 +0530

    TSM8889-1887: Making sonar integration available

---
+++ build/analysis/sonar/scanner.js
@@ -0,0 +1,34 @@
+/**
+ * this file is holding the logic related to publishing sonar scanning
+ * to the server
+ */
+const sonarqubeScanner = require("sonarqube-scanner");
+
+/**
+ * these are the options needed by sonar scanning module in order to display
+ * the report
+ */
+const configuration = {
+  serverUrl: "http://sonar-noi.barco.com:9000/",
+  options: {
+    "sonar.projectName": "collaboration-wall-manager",
+    "sonar.projectVersion": "1.0.1",
+    "sonar.sources": "app",
+    "sonar.tests": "test",
+    "sonar.typescript.lcov.reportPaths": "build/coverage/lcov.info"
+  }
+};
+
+/**
+ * Scanning finished callback
+ * @method scanningCallback
+ */
+const scanningCallback = () => {
+  console.log("info from code");
+  console.log(arguments);
+};
+
+/**
+ * Invoke the scanning
+ */
+sonarqubeScanner(configuration, scanningCallback);
--- build/configuration/karma.config.js
+++ build/configuration/karma.config.js
@@ -1,138 +1,144 @@
 module.exports = function (config) {
-    config.set({
-        basePath: "../..",
-        proxies: {
-            "/settings/language/resources/": "/base/app/resources/",
-            "/settings/language/icon_barco.png": "/base/app/icon_barco.png",
-            "/display-panel/resources/": "/base/app/resources/",
-            "/display-panel/icon_barco.png": "/base/app/icon_barco.png",
-            "/resources/": "/base/app/resources/",
-            "/icon_barco.png": "/base/app/icon_barco.png",
-            "/home/resources/": "/base/app/resources/",
-            "/home/icon_barco.png": "/base/app/icon_barco.png",
- 			"/display_snapshot.jpg": "/base/app/resources/images/display_snapshot.jpg",
-             "/home/display_snapshot.jpg": "/base/app/resources/images/display_snapshot.jpg"
+  config.set({
+    basePath: "../..",
+    proxies: {
+      "/settings/language/resources/": "/base/app/resources/",
+      "/settings/language/icon_barco.png": "/base/app/icon_barco.png",
+      "/display-panel/resources/": "/base/app/resources/",
+      "/display-panel/icon_barco.png": "/base/app/icon_barco.png",
+      "/resources/": "/base/app/resources/",
+      "/icon_barco.png": "/base/app/icon_barco.png",
+      "/home/resources/": "/base/app/resources/",
+      "/home/icon_barco.png": "/base/app/icon_barco.png",
+      "/display_snapshot.jpg": "/base/app/resources/images/display_snapshot.jpg",
+      "/home/display_snapshot.jpg": "/base/app/resources/images/display_snapshot.jpg"
+    },
+    frameworks: [
+      'jasmine',
+      'karma-typescript'
+    ],
+    reporters: [
+      "progress",
+      "karma-typescript",
+      "coverage",
+      "dots"
+    ],
+    htmlReporter: {
+      outputDir: "build/reports",
+      reportName: "karma-test-report",
+      preserveDescribeNesting: false,
+      foldAll: false,
+    },
+    preprocessors: {
+      '**/*.ts': [
+        'karma-typescript', "coverage"
+      ]
+      , 'app/*.*scss': ['scss']
+      , 'app/**/*.*scss': ['scss']
+    },
+    autoWatch: true,
+    files: [
+      {
+        pattern: 'test/base.ts'
+      },
+      {
+        pattern: 'app/**/*.ts'
+      },
+      {
+        pattern: 'test/**/*.ts'
+      },
+      {
+        pattern: 'app/**/*.png',
+        included: false
+      },
+      {
+        pattern: 'app/**/*.jpg',
+        included: false
+      },
+      {
+        pattern: 'app/**/*.svg',
+        included: false
+      },
+      {
+        pattern: "./app/i18n/*.json",
+        watched: true,
+        served: true,
+        included: false
+      },
+      {
+        pattern: 'app/*.*scss',
+        watched: true,
+        included: true,
+        served: true
+      },
+      {
+        pattern: 'app/main.ts',
+        watched: false,
+        included: false,
+        served: false
+      }
+    ],
+    karmaTypescriptConfig: {
+      exclude: ["broken"],
+      tsconfig: './tsconfig.json',
+      coverageOptions: {
+        instrumentation: true,
+        exclude: [/\.(spec|mock|stub).ts$/i, /deps.ts/, /main.ts/, /base.ts/]
+      },
+      reports: {
+        lcovonly: {
+          "directory": "build",
+          "subdirectory": "coverage",
+          "filename": "lcov.info",
+        }
+      },
+      remapOptions: {
+        warn: function (message) {
+          console.warn(message);
+        }
+      },
+      bundlerOptions: {
+        entrypoints: /base\.ts|\.spec\.ts$/,
+        resolve: {
+          extensions: [".js", ".json"],
+          directories: ["node_modules"]
         },
-        frameworks: [
-            'jasmine',
-            'karma-typescript'
-        ],
-        reporters: [
-            "progress",
-            "karma-typescript",
-            "html"
-        ],
-        htmlReporter: {
-            outputDir: "build/reports",
-            reportName: "karma-test-report",
-            preserveDescribeNesting: false,
-            foldAll: false,
-        },
-        preprocessors: {
-            '**/*.ts': [
-                'karma-typescript'
+        transforms: [
+          require('karma-typescript-es6-transform')({
+            presets: ['es2015', 'stage-0'],
+            extensions: ['.ts', '.js'],
+            plugins: [
+              ["transform-runtime", {
+                regenerator: true,
+                polyfill: true
+              }]
             ]
-            ,'app/*.*scss': ['scss']
-            ,'app/**/*.*scss': ['scss']
-        },
-        autoWatch: true,
-        files: [
-            {
-                pattern: 'test/base.ts'
-            },
-            {
-                pattern: 'app/**/*.ts'
-            },
-            {
-                pattern: 'test/**/*.ts'
-            },
-            {
-                pattern: 'app/**/*.png',
-                included: false
-            },
-            {
-                pattern: 'app/**/*.jpg',
-                included: false
-            },
-            {
-                pattern: 'app/**/*.svg',
-                included: false
-            },
-            {
-                pattern: "./app/i18n/*.json",
-                watched: true,
-                served: true,
-                included: false
-            },
-            {
-                pattern: 'app/*.*scss',
-                watched: true,
-                included: true,
-                served: true
-            },
-            {
-                pattern: 'app/main.ts',
-                watched: false,
-                included: false,
-                served: false
+          }),
+          /**
+           * Custom transformer to resolved the issue related
+           * to importing the css from server.
+           */
+          function (context, callback) {
+            if (context.module.indexOf('.scss') !== -1) {
+              context.source = context.source.replace(
+                '@import "variables.global";',
+                '@import "/base/app/_variables.global.css"'
+              );
+              return callback(undefined, true);
             }
-        ],
-        karmaTypescriptConfig: {
-            exclude: ["broken"],
-            tsconfig: './tsconfig.json',
-            coverageOptions: {
-                instrumentation: true,
-                exclude: [/\.(spec|mock|stub).ts$/i, /deps.ts/, /main.ts/, /base.ts/]
-            },
-            remapOptions: {
-                warn: function(message){
-                    console.warn(message);
-                }
-            },
-            reports: {
-                "html": {
-                    "directory": "build/reports",
-                    "subdirectory": "coverage-report",
-                    "filename": "coverage"
-                }
-            },
-            bundlerOptions: {
-                entrypoints: /base\.ts|\.spec\.ts$/,
-                resolve: {
-                    extensions: [".js", ".json"],
-                    directories: ["node_modules"]
-                },
-                transforms: [
-                    require('karma-typescript-es6-transform')({
-                        presets: ['es2015', 'stage-0'],
-                        extensions: ['.ts', '.js'],
-                        plugins: [
-                            ["transform-runtime", {
-                                regenerator: true,
-                                polyfill: true
-                            }]
-                        ]
-                    }),
-                    /**
-                     * Custom transformer to resolved the issue related
-                     * to importing the css from server.
-                     */
-                    function(context, callback) {
-                        if(context.module.indexOf('.scss') !== -1) {
-                            context.source = context.source.replace(
-                                '@import "variables.global";',
-                                '@import "/base/app/_variables.global.css"'
-                            );
-                            return callback(undefined, true);
-                        }
-                        return callback(undefined, false);
-                    }
-                ]
-            }
-        },
-        logLevel: config.LOG_INFO,
-        browsers: [
-            'Chrome'
+            return callback(undefined, false);
+          }
         ]
-    });
+      }
+    },
+    logLevel: config.LOG_INFO,
+    browsers: [
+      'Chrome'
+    ],
+    /**
+     * @TARJU - Discuss
+     * You would never turn this off and commit
+     */
+    singleRun: true
+  });
 };
