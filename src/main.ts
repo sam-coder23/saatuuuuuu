8874a40dddef0675eb62e88b19f41b97d819d4a3
MDSharique31 <shariquevivo@gmail.com>
2026-07-11 01:27:11 +0530

    Upgrade Angular to 21

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@
  * This class intialize CMS LAUNCHPAD MODULE
  */
 
-import "core-js/features/reflect";
+import 'core-js/features/reflect';
 // import "zone.js/dist/zone";
 
-import { enableProdMode } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { CmsLaunchapadModule } from "./app/launchpad/cms-launchpad.module";
+import { CmsLaunchapadModule } from './app/launchpad/cms-launchpad.module';
 
 enableProdMode();
-platformBrowserDynamic().bootstrapModule(CmsLaunchapadModule);
+platformBrowserDynamic().bootstrapModule(CmsLaunchapadModule, { applicationProviders: [provideZoneChangeDetection()] });
